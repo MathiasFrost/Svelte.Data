77100822e55bb7a8d1c3de5381461e9cf149be2f
Mathias Løken <lakavi@mailfence.com>
2022-11-27 16:25:05 +0100

    add

---
+++ src/lib/store/shared/deepCopy.ts
@@ -0,0 +1,32 @@
+import {isObject, type UnknownObject} from "./UnknownObject";
+
+/** */
+export function deepCopy<T>(something: T): T {
+	if (typeof something === "string") {
+		return something;
+	}
+	if (Array.isArray(something)) {
+		return something.map((el) => deepCopy(el)) as T;
+	}
+	if (isObject(something)) {
+		const copy: UnknownObject = {};
+		const keys = Object.keys(something);
+		for (const key of keys) {
+			const el = something[key];
+			if (Array.isArray(el)) {
+				copy[key] = el.map((e) => deepCopy(e));
+			}
+			// Special treatment for Date
+			else if (el instanceof Date) {
+				copy[key] = new Date(el.valueOf());
+			} else if (typeof el === "object" && el !== null) {
+				copy[key] = deepCopy(el);
+			} else {
+				copy[key] = el;
+			}
+		}
+		return copy as T;
+	}
+
+	return something;
+}
