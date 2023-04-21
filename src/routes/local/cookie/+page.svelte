04652bf5c40fc455eb9258abe09963ade0f676a1
Mathias Løken <lakavi@mailfence.com>
2023-04-21 23:41:41 +0200

    loacl

--- src/routes/local/cookie/+page.svelte
+++ src/routes/local/cookie/+page.svelte
@@ -1,50 +1,93 @@
 <script lang="ts">
 	import { browser } from "$app/environment";
 
-	type ValueBuilderResult<T> = {
-		get value(): T;
-		set value(val: T);
+	const promise = () =>
+		new Promise<string>((resolve) => {
+			window.setTimeout(() => {
+				console.log("sa");
+				resolve("hah");
+			}, 600);
+		});
+
+	type StorageValueBuilderResult<T> = {
+		push: (value: T) => void;
+		pull: () => void;
+		reset: () => void;
 	};
 
-	class ValueBuilder<T> {
+	class StorageValueBuilder<T> {
 		private initialValue?: T;
 
-		public withInitialValue(value: T): ValueBuilder<T> {
+		public withInitialValue(value: T): StorageValueBuilder<T> {
 			this.initialValue = value;
 			return this;
 		}
 
-		public asObject(): ValueBuilderResult<T> {
-			let _value: T;
-			if (typeof window !== "undefined") {
-				const str = window.sessionStorage.getItem("test");
-				if (str !== null) _value = str as T;
-			}
+		private setter?: (value: T) => void;
 
-			if (typeof _value === "undefined") {
-				if (typeof this.initialValue === "undefined") throw new Error("");
-				_value = this.initialValue;
-			}
-			const o = {
-				get value() {
-					return _value;
-				},
-				set value(val) {
-					if (typeof window !== "undefined") window.sessionStorage.setItem("test", val as string);
-					_value = val;
+		public withSetter(setter: (value: T) => void): StorageValueBuilder<T> {
+			this.setter = setter;
+			return this;
+		}
+
+		private promise?: () => Promise<T>;
+
+		public fromPromise(promise: () => Promise<T>): StorageValueBuilder<T> {
+			this.promise = promise;
+			return this;
+		}
+
+		public asObject(): StorageValueBuilderResult<T> {
+			const push = (val: T) => {
+				if (typeof window !== "undefined") window.sessionStorage.setItem("test", val as string);
+			};
+
+			const pull = () => {
+				if (typeof window !== "undefined") {
+					const str = window.sessionStorage.getItem("test");
+					if (str !== null) {
+						this.setter?.(str as T);
+					} else {
+						window.sessionStorage.setItem("test", this.initialValue as string);
+						if (typeof this.promise !== "undefined") this.promise().then(sync);
+					}
 				}
 			};
 
-			if (browser) window.setTimeout(() => (o.value = "test" as T), 600);
-			return o;
+			function sync(value: T) {
+				push(value);
+				pull();
+			}
+
+			const reset = () => {
+				if (typeof this.initialValue === "undefined") {
+					window.sessionStorage.removeItem("test");
+				} else {
+					push(this.initialValue);
+					this.setter?.(this.initialValue);
+				}
+				if (typeof this.promise !== "undefined") this.promise().then(sync);
+			};
+
+			pull();
+			return { pull, push, reset };
 		}
 	}
 
-	const value = new ValueBuilder<string>().withInitialValue("").asObject();
+	let str = "asd";
+	const { pull, push, reset } = new StorageValueBuilder<string>()
+		.withInitialValue(str)
+		.fromPromise(promise)
+		.withSetter((val) => (str = val))
+		.asObject();
+	$: push(str);
 </script>
 
 <h1>Svelte.Data</h1>
 
 <h2>ValueBuilder with cookie sync</h2>
 
-<input type="text" bind:value={value.value} />
+<input type="text" bind:value={str} />
+
+<button on:click={pull}> pull </button>
+<button on:click={reset}> reset </button>
