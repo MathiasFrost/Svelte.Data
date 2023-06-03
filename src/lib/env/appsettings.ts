fda6490bd2a51448fadc2e67f1e85828da45cf7e
Mathias Løken <lakavi@mailfence.com>
2023-06-03 12:20:17 +0200

    Like ASP.NET Core

--- src/lib/env/appsettings.ts
+++ src/lib/env/appsettings.ts
@@ -2,10 +2,92 @@ import { cloneViaSerialization } from "$lib/clone.js";
 import fs from "fs";
 
 /** */
-enum JsonType {
-	value,
-	object,
-	array
+type JsonType = "null" | "value" | "object" | "array";
+
+/** Read file and return a JsonObject. If it doesn't exist return en empty JsonObject */
+const parseJsonFile = (filename: string) => {
+	if (fs.existsSync(filename)) {
+		try {
+			return JSON.parse(fs.readFileSync(filename, "utf-8"));
+		} catch (e) {
+			console.error(`Unable to parse ${filename}`);
+			throw e;
+		}
+	} else {
+		return {};
+	}
+};
+
+/** Find out which prop type the JsonNode is */
+const jsonType = (node: unknown): JsonType => {
+	switch (typeof node) {
+		case "number":
+		case "string":
+		case "boolean":
+		case "bigint":
+			return "value";
+		case "object":
+			if (node === null) return "null";
+			if (Array.isArray(node)) return "array";
+			return "object";
+		default:
+			throw new Error("JsonNode could not be cast to anything");
+	}
+};
+
+/** TODOC */
+function isValue(node: unknown): node is number | string | boolean | bigint {
+	return jsonType(node) === "value";
+}
+
+/** TODOC */
+function isObject(node: unknown): node is Record<string, unknown> {
+	return jsonType(node) === "object";
+}
+
+/** TODOC */
+function isArray(node: unknown): node is Record<string, unknown>[] {
+	return jsonType(node) === "array";
+}
+
+/// Layer b on top of a
+function merge(a: unknown, b: unknown): void {
+	if (isObject(a) && isObject(b)) {
+		for (const keyB of Object.keys(b)) {
+			const valA = a[keyB];
+			const valB = b[keyB];
+
+			if (typeof valA !== "undefined") {
+				if (isValue(valA)) a[keyB] = cloneViaSerialization(valB); // If a is value, replace a with b
+				else if (isObject(valA) && isObject(valB)) merge(valA, valB); // If both are objects merge them recursively
+				else if (isArray(valA) && isArray(valB)) merge(valA, valB); // If both are objects merge them recursively
+				else if (isObject(valB)) a[keyB] = cloneViaSerialization(valB); // If types are different, just overwrite
+				else if (isArray(valB)) a[keyB] = cloneViaSerialization(valB); // If types are different, just overwrite
+				else if (isValue(valB)) a[keyB] = cloneViaSerialization(valB); // If types are different, just overwrite
+			} else {
+				// If a does not have b, simply add b to a
+				a[keyB] = cloneViaSerialization(valB);
+			}
+		}
+	} else if (isArray(a) && isArray(b)) {
+		let i = 0;
+		for (const elB of b) {
+			if (a.length > i) {
+				const elA = a[i];
+
+				if (isValue(elA)) a[i] = cloneViaSerialization(elB); // If a is value, replace a with b
+				else if (isObject(elA) && isObject(elB)) merge(elA, elB); // If both are objects merge them recursively
+				else if (isArray(elA) && isArray(elB)) merge(elA, elB); // If both are objects merge them recursively
+				else if (isObject(elB)) a[i] = cloneViaSerialization(elB); // If types are different, just overwrite
+				else if (isArray(elB)) a[i] = cloneViaSerialization(elB); // If types are different, just overwrite
+				else if (isValue(elB)) a[i] = cloneViaSerialization(elB); // If types are different, just overwrite
+			} else {
+				// If a does not have b, simply add b to a
+				a.push(cloneViaSerialization(elB));
+			}
+			i++;
+		}
+	}
 }
 
 /** Load appsettings files, merge them and return a JsonObject */
@@ -13,86 +95,6 @@ export function loadAppsettings(): void {
 	if (typeof process === "undefined") {
 		console.warn("Tried to call loadAppsettings client-side");
 		return;
-	}
-
-	/** Read file and return a JsonObject. If it doesn't exist return en empty JsonObject */
-	const parseJsonFile = (filename: string) => {
-		if (fs.existsSync(filename)) return JSON.parse(fs.readFileSync(filename, "utf-8"));
-		else return {};
-	};
-
-	/** Find out which prop type the JsonNode is */
-	const jsonType = (node: unknown) => {
-		if (Array.isArray(node)) return JsonType.array;
-		switch (typeof node) {
-			case "number":
-			case "string":
-			case "boolean":
-				return JsonType.value;
-			case "object":
-				return node === null ? JsonType.value : JsonType.object;
-			default:
-				throw new Error("JsonNode could not be cast to anything");
-		}
-	};
-
-	/** Try to find the value of a key */
-	const tryFind = (key: string, env: Record<string, unknown>) => (key in env ? env[key] : null);
-
-	/** Check if JsonArray array contains a JsonNode */
-	const exists = (arr: Record<string, unknown>[], node: Record<string, unknown>) => arr.some((x) => JSON.stringify(x) === JSON.stringify(node));
-
-	/** Add array elements from a that does not exist in b */
-	function mergeArray(a: Record<string, unknown>[], b: Record<string, unknown>[]) {
-		for (const itemA of a) {
-			const copyA = cloneViaSerialization(itemA);
-			if (exists(b, copyA)) continue;
-			b.push(copyA);
-		}
-	}
-
-	/** Add the values from a that does not exist in b */
-	function merge(a: Record<string, unknown>, b: Record<string, unknown>): void {
-		for (const keyA of Object.keys(a)) {
-			const propA = a[keyA];
-			const propB = tryFind(keyA, b);
-			switch (jsonType(propA)) {
-				case JsonType.value:
-					if (propB !== null) b[keyA] = cloneViaSerialization(propA);
-					break;
-				case JsonType.object:
-					if (propB !== null) b[keyA] = cloneViaSerialization(propA);
-					switch (jsonType(propB)) {
-						case JsonType.value:
-							break;
-						case JsonType.object:
-							merge(propA as Record<string, unknown>, propB as Record<string, unknown>);
-							break;
-						case JsonType.array:
-							break;
-						default:
-							break;
-					}
-					break;
-				case JsonType.array:
-					if (propB !== null) b[keyA] = cloneViaSerialization(propA);
-					switch (jsonType(propB)) {
-						case JsonType.value:
-							break;
-						case JsonType.object:
-							mergeArray(propA as Record<string, unknown>[], propB as Record<string, unknown>[]);
-							break;
-						case JsonType.array:
-							break;
-						default:
-							break;
-					}
-					break;
-
-				default:
-					break;
-			}
-		}
 	}
 
 	const svelteKitEnv =
@@ -110,14 +112,14 @@ export function loadAppsettings(): void {
 	function flatten(parent: string, o: Record<string, unknown>): void {
 		for (const key of Object.keys(o)) {
 			switch (jsonType(o[key])) {
-				case JsonType.value:
-					console.log(`${parent}__${key}: ${o[key]}`);
-					process.env[`${parent}__${key}`] = `${o[key]}`;
+				case "value":
+					console.log(`${parent}${key}: ${o[key]}`);
+					process.env[`${parent}${key}`] = `${o[key]}`;
 					break;
-				case JsonType.object:
-					flatten(key, o[key] as Record<string, unknown>);
+				case "object":
+					flatten(key + "__", o[key] as Record<string, unknown>);
 					break;
-				case JsonType.array:
+				case "array":
 					for (let i = 0; i < (o[key] as Record<string, unknown>[]).length; i++) {
 						const el = (o[key] as Record<string, unknown>[])[i];
 						flatten(`${key}__0`, el);
