fc09c01920999b1e078d6d21000118eb58d06a5a
Mathias Løken <77194821+MathiasFrost@users.noreply.github.com>
2023-06-08 19:37:03 +0200

    Postprocess right after (#51)
    And supply information about whether the response is null and allowed to be

--- src/lib/http/Postprocess.ts
+++ src/lib/http/Postprocess.ts
@@ -1,2 +1,4 @@
-/** Modify Response right after response is received */
-export type Postprocess = (response: Response) => Promise<void>;
+/** Modify Response right after response is received
+ * @param response The unaltered response from the fetch request
+ * @param nullable True if this request accepts null result and received a status code indicating null result  */
+export type Postprocess = (response: Response, nullable: boolean) => Promise<void>;
--- src/sandbox/http/TestHTTP.ts
+++ src/sandbox/http/TestHTTP.ts
@@ -1,11 +1,21 @@
 import type { Fetch } from "$lib/http/Fetch.js";
 import { HTTPClient } from "$lib/http/HTTPClient.js";
 import { WeatherForecast } from "$sandbox/models/WeatherForecast.js";
+import { indefinitePromise } from "$lib/async/index.js";
+import { ensureObject, ensureString } from "$lib/types/index.js";
 
 /** @static */
 export class TestHTTP {
 	/** */
-	private static client = new HTTPClient("http://localhost:5173/api/", HTTPClient.backendInit());
+	private static client = new HTTPClient("http://localhost:5173/api/", HTTPClient.backendInit(), void 0, async (response, nullable) => {
+		console.log(response.status);
+		if (!nullable && response.status === 401) {
+			if (typeof window !== "undefined") {
+				window.location.reload();
+			}
+			await indefinitePromise();
+		}
+	});
 
 	/** */
 	public static async getForecasts(fetch?: Fetch): Promise<WeatherForecast[]> {
@@ -13,5 +23,20 @@ export class TestHTTP {
 			.get("weatherforecast")
 			.withFetch(fetch)
 			.fromJSONArray((something) => new WeatherForecast(something));
+	}
+
+	public static async getUser(): Promise<{ name: string } | null> {
+		return await this.client
+			.get("user")
+			.withNullStatus(401)
+			.fromJSONObjectNullable<{ name: string }>((something) => ({ name: ensureString(ensureObject(something).name) }));
+	}
+
+	public static async getTest(): Promise<string> {
+		return await this.client.get("test").fromString();
+	}
+
+	public static async postUseR(): Promise<void> {
+		await this.client.post("user").fetch();
 	}
 }
