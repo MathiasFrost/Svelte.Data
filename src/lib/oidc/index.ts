d2213d6ebc94a490fc680e97cf5d78e998ed4629
Mathias Løken <lakavi@mailfence.com>
2023-10-07 12:00:31 +0200

    Format and lint

--- src/lib/oidc/index.ts
+++ src/lib/oidc/index.ts
@@ -1,2 +1,4 @@
 export type { OIDCConfiguration } from "$lib/oidc/OIDCConfiguration.js";
 export { OIDCManager } from "$lib/oidc/OIDCManager.js";
+export { OIDCMessage } from "$lib/oidc/OIDCMessage.js";
+export { OIDCConfigurationProvider } from "$lib/oidc/OIDCConfigurationProvider.js";
