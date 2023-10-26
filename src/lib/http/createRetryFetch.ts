16cc6caeba499dc9c9f961f17b998cfd041269fa
Mathias Løken <lakavi@mailfence.com>
2023-10-26 17:08:07 +0200

    Allow using server-fetch on retry fetcher (not for OIDC fetch tho)

--- src/lib/http/createRetryFetch.ts
+++ src/lib/http/createRetryFetch.ts
@@ -1,9 +1,11 @@
 import type { Fetch } from "$lib/http/Fetch.js";
 
-export function createRetryFetch(maxRetries: number): Fetch {
+export function createRetryFetch(maxRetries: number, fetch?: Fetch): Fetch {
 	return async (requestInfo, requestInit, nullStatusCodes) => {
+		fetch ??= window.fetch;
+
 		// Perform the first fetch without delay
-		let response = await window.fetch(requestInfo, requestInit);
+		let response = await fetch(requestInfo, requestInit);
 
 		let retryCount = 0;
 		while (!nullStatusCodes?.includes(response.status) && [500, 502, 503, 504, 507, 429, 425, 408].includes(response.status) && retryCount++ < maxRetries) {
@@ -11,7 +13,7 @@ export function createRetryFetch(maxRetries: number): Fetch {
 			const delay = (Math.pow(2, retryCount) + Math.random() * 0.5) * 1_000;
 
 			await new Promise((resolve) => setTimeout(resolve, delay));
-			response = await window.fetch(requestInfo, requestInit);
+			response = await fetch(requestInfo, requestInit);
 		}
 		return response;
 	};
