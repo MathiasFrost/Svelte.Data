b395b2a8a5bca8a98d7a64e28047c0adaa843a82
Mathias Løken <lakavi@mailfence.com>
2023-10-28 07:39:44 +0200

    More explicit name

--- src/lib/async/utility.ts
+++ src/lib/async/utility.ts
@@ -5,9 +5,9 @@ export function indefinitePromise<T>(): Promise<T> {
 }
 
 /** Utility function for ensuring that promise is only called client-side */
-export function browserPromise<T>(promise: () => Promise<T>): Promise<T> {
+export function browserPromise<T>(promiseFactory: () => Promise<T>): Promise<T> {
 	if (typeof window === "undefined") return indefinitePromise();
-	else return promise();
+	else return promiseFactory();
 }
 
 /** Utility type for an object containing both the value and it's promise representation */
