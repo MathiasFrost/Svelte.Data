698e66657f0456df7f0d9a9df463b186047fc9af
Mathias Løken <lakavi@mailfence.com>
2024-01-09 21:35:20 +0100

    Try without custom option

--- src/lib/html/HTMLEditor.svelte
+++ src/lib/html/HTMLEditor.svelte
@@ -83,11 +83,7 @@
 
 		ast.forEach((a) => build(a));
 
-		return sanitizeString(builder);
-	}
-
-	function sanitizeString(str: string): string {
-		return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#x27;").replace(/\//g, "&#x2F;");
+		return builder;
 	}
 </script>
 
