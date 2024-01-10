9004873781304df5567df1e401d5ac40a9f7c8d3
Mathias Løken <lakavi@mailfence.com>
2024-01-10 18:55:13 +0100

    Ok ok

--- src/lib/select/index.ts
+++ src/lib/select/index.ts
@@ -1,3 +1,2 @@
 export type { SvelteEnhancedSelectElement } from "$lib/select/SvelteEnhancedSelectElement.js";
-export type { SvelteEnhancedOptionElement } from "$lib/select/SvelteEnhancedOptionElement.js";
 export { SelectHelper } from "$lib/select/SelectHelper.js";
